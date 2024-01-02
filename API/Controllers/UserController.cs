271e82913e9bb137779f7e7a833320f32ad613af
Nathanael de Souza Leite <Nathanael.leite@datalinkbpo.com.br>
2024-01-02 16:53:13 -0300

    Separação dos projestos/arquitetura

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Login(string username, string password) {
+            return Ok(new { response = "Bem vindo "+ username + "!!!"});
+        }
+    }
+}
